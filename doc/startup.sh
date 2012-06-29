33ce106c587754e92e3df7b3bf47240f46bf230f
Jeff Murphy <jcmurphy@jeffmurphy.org>
2012-06-29 10:44:55 -0400

    single host hadoop conf files

--- doc/startup.sh
+++ doc/startup.sh
@@ -1,8 +1,31 @@
-   src/hbase-0.92.1/bin/hbase-daemon.sh stop thrift
-   src/hbase-0.92.1/bin/stop-hbase.sh 
-   src/hadoop-1.0.3/bin/stop-all.sh 
-   src/hadoop-1.0.3/bin/start-all.sh 
+#!/bin/sh 
+
+# if you are using cloudera
+ /etc/init.d/hbase-thrift stop
+ /etc/init.d/hbase-master stop
+ /etc/init.d/hadoop-hdfs-datanode stop
+ /etc/init.d/hadoop-hdfs-namenode stop
+ 
+ /etc/init.d/hadoop-hdfs-datanode start
+ /etc/init.d/hadoop-hdfs-namenode start
+
+ sleep 5
+
+ hdfs fsck /
+ sudo -u hdfs hdfs dfsadmin -safemode leave
+  
+ /etc/init.d/hbase-master start
+ /etc/init.d/hbase-thrift start
+
+
+exit 0
+# if you are using apache tarballs
+
+   $HOME/src/hbase-0.92.1/bin/hbase-daemon.sh stop thrift
+   $HOME/src/hbase-0.92.1/bin/stop-hbase.sh 
+   $HOME/src/hadoop-1.0.3/bin/stop-all.sh 
+   $HOME/src/hadoop-1.0.3/bin/start-all.sh 
    $HOME/src/hadoop-1.0.3/bin/hadoop dfsadmin -safemode leave
    $HOME/src/hadoop-1.0.3/bin/hadoop fsck /
-   src/hbase-0.92.1/bin/start-hbase.sh 
-   src/hbase-0.92.1/bin/hbase-daemon.sh start thrift
+   $HOME/src/hbase-0.92.1/bin/start-hbase.sh 
+   $HOME/src/hbase-0.92.1/bin/hbase-daemon.sh start thrift
