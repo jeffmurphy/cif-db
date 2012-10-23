1f6e4c8916644bff4032682498f4fae5b4ebe95d
jeff murphy <jcmurphy@jeffmurphy.org>
2012-10-23 15:17:19 -0400

    misc

--- src/poc-db.py
+++ src/poc-db.py
@@ -11,6 +11,10 @@ import json
 import getopt
 import socket
 import happybase
+import hashlib
+import struct
+import traceback
+import re
 
 # adjust to match your $PREFIX if you specified one
 # default PREFIX = /usr/local
@@ -22,6 +26,10 @@ import control_pb2
 import RFC5070_IODEF_v1_pb2
 import MAEC_v2_pb2
 import cifsupport
+
+sys.path.append('../../cif-router/poc')
+from CIF.CtrlCommands.Clients import *
+from CIF.Foundation import Foundation
 
 print "cif-db proof of concept"
 
@@ -40,88 +48,7 @@ Attach to cif-router ROUTER:
 
 
 
-def ctrlsocket(myname, cifrouter):
-    # Socket to talk to cif-router
-    req = context.socket(zmq.REQ);
-    req.setsockopt(zmq.IDENTITY, myname)
-    req.connect('tcp://' + cifrouter)
-    return req
 
-def subscribersocket(publisher):
-    # Socket to publish from
-    print "Creating subscriber socket and connecting to " + publisher
-    subscriber = context.socket(zmq.SUB)
-    subscriber.connect('tcp://' + publisher)
-    subscriber.setsockopt(zmq.SUBSCRIBE, '')
-    return subscriber
-
-def unregister(req, apikey, cifrouter, myid):
-    print "Send UNREGISTER to cif-router (" + cifrouter + ")"
-    
-    msg = control_pb2.ControlType()
-    msg.version = msg.version # required
-    msg.apikey = apikey
-    msg.type = control_pb2.ControlType.COMMAND
-    msg.command = control_pb2.ControlType.UNREGISTER
-    msg.dst = 'cif-router'
-    msg.src = myid
-    msg.apikey = apikey;
-    
-    req.send(msg.SerializeToString())
-    
-    reply = req.recv()
-    msg.ParseFromString(reply)
-    
-    try:
-        cifsupport.versionCheck(msg)
-    except Exception as e:
-        print "Received message was bad: ", e
-    else:
-        print "\tGot reply."
-        if msg.status == control_pb2.ControlType.SUCCESS:
-            print "\t\tunregistered successfully"
-        else:
-            print "\t\tnot sure? " + msg.status
-
-def register(apikey, req, myip, myid, cifrouter):
-    routerport = 0
-    routerpubport = 0
-    
-    print "Send REGISTER to cif-router (" + cifrouter + ")"
-    
-    msg = control_pb2.ControlType()
-    msg.version = msg.version # required
-    msg.apikey = apikey
-    msg.type = control_pb2.ControlType.COMMAND
-    msg.command = control_pb2.ControlType.REGISTER
-    msg.dst = 'cif-router'
-    msg.src = myid
-    print " Sending REGISTER: " #, msg
-    
-    req.send_multipart([msg.SerializeToString(), ''])
-    reply = req.recv_multipart();
-
-    print " REGISTER Got reply: " #, reply
-    msg.ParseFromString(reply[0])
-
-    #print " REGISTER decoded: ", msg
-    routerport = msg.registerResponse.REQport
-    routerpubport = msg.registerResponse.PUBport
-    if msg.status == control_pb2.ControlType.SUCCESS:
-        print "  registered successfully"
-        return (routerport, routerpubport)
-    elif msg.status == control_pb2.ControlType.DUPLICATE:
-        print "  already registered?"
-        return (routerport, routerpubport)
-    else:
-        print "  register failed."
-
-    return (0,0)
-
-def ctrlc(req, apikey, cifrouter, myid):
-    print "Shutting down."
-    unregister(req, apikey, cifrouter, myid)
-    sys.exit(0)
     
 def usage():
     print "\
@@ -130,25 +57,52 @@ def usage():
     #     -r  cif-router hostname:port\n\
     #     -m  my name\n"
     
-def ctrl(rep, controlport):
-    print "Creating control socket on :" + controlport
-    # Socket to accept control requests on
-    rep = context.socket(zmq.REP);
-    rep.bind('tcp://*:' + controlport);
 
 def HBConnection(host):
     c = happybase.Connection(host)
     t = c.tables()
     print "found tables: ", t
     if not "cif_idl" in t:
-        print "missing cif_idl table"
+        raise Exception("missing cif_idl table")
     if not "cif_objs" in t:
-        print "missing cif_objs table"
-        
-def writeToDb(msg):
-    print "Write message to db"
+        raise Exception("missing cif_objs table")
+    return c
+
+"""
+Given a msg object, we want to record its IDL (for posterity)
+to cif_idl if it hasn't been already. We then write the actual object
+to cif_objs.
+
+#     rowkey $salt$timestamp$hash   (eg "<2 byte salt><8 byte timestamp><16 byte md5>")
+#     cf:$submsgtype     (eg cf:RFC5070-IODEF-v1=object)
+
+
+
+"""
+
+def saveIDL(cif_idl, sr):
+    #bot = sr.baseObjectType;
+    bot = re.sub('_', '-', sr.baseObjectType)
+    fn = cifsupport.installBase() + "/" + bot + ".proto"
+    print "IDL should be: " + fn
     
-global req
+def writeToDb(cif_objs, cif_idl, sr):
+    print "\tWrite message(s) to db: "  + str(sr.baseObjectType)
+    ts = int(time.time()) # ignore fractional seconds
+    md5 = hashlib.md5()
+    md5.update(sr.SerializeToString())
+    hash = md5.digest()
+    colspec = "cf:" + str(sr.baseObjectType)
+    salt = 0xFF00
+    try:
+        saveIDL(cif_idl, sr)
+        rowid = struct.pack("II16s", salt, ts, hash)
+        cif_objs.put(rowid, {colspec: sr.data})
+        print "\tput: rowid:" + rowid.encode('hex') + " " + colspec + " "
+    except struct.error, err:
+        print "Failed to pack rowid: ", err
+    
+    
 
 try:
     opts, args = getopt.getopt(sys.argv[1:], 'c:r:m:h')
@@ -160,6 +114,7 @@ except getopt.GetoptError, err:
 controlport = "5656"
 cifrouter = "sdev.nickelsoft.com:5555"
 myid = "poc-db"
+apikey = "1234567890abcdef"
 
 for o, a in opts:
     if o == "-c":
@@ -174,46 +129,67 @@ for o, a in opts:
 
 myip = socket.gethostbyname(socket.gethostname()) # has caveats
 
-print "ZMQ::Context"
+global cf
+cf = Foundation({'apikey' : apikey,
+                 'myip'   : myip,
+                 'cifrouter' : cifrouter,
+                 'controlport' : controlport,
+                 'myid' : myid,
+                 'routerid' : "cif-router"
+                 })
 
-context = zmq.Context()
-myname = myip + ":" + controlport + "|" + myid
 
 try:
     print "Connect to HBase"
     connection = HBConnection('localhost')
-    
-    print "Register with " + cifrouter + " (req->rep)"
-    req = ctrlsocket(myname, cifrouter)
-    (routerport, routerpubport) = register(req, cifrouter)
-    routerhname = cifrouter.split(':')
+    cif_objs = connection.table('cif_objs').batch(batch_size=5) # set very low for development, set to 1000+ for test/qa/prod
+    cif_idl = connection.table('cif_idl')
 
-    subscriber = subscribersocket(routerhname[0] + ":" + str(routerpubport))
+    print "Register with " + cifrouter + " (req->rep)"
+    req = cf.ctrlsocket()
+    # apikey, req, myip, myid, cifrouter
+    (routerport, routerpubport) = cf.register()
+
+    subscriber = cf.subscribersocket()
     
     time.sleep(1) # wait for router to connect, sort of lame but see this a lot in zmq code
     
     while True:
         msg = msg_pb2.MessageType()
         msg.ParseFromString(subscriber.recv())
+        
         if msg.type == msg_pb2.MessageType.SUBMISSION and len(msg.submissionRequest) > 0:
-            print "Got msg with: ", msg.submissionRequest[0].baseObjectType
-            writeToDb(msg)
+            print "Got a SUBMISSION. Saving."
+            for i in range(0, len(msg.submissionRequest)):
+                writeToDb(cif_objs, cif_idl, msg.submissionRequest[i])
+        
+        elif msg.type == msg_pb2.MessageType.QUERY and len(msg.queryRequest) > 0:
+            print "Got a QUERY. Processing."
+            for i in range(0, len(msg.submissionRequest)):
+                qreply = readFromDb(msg.queryRequest[0])
+                msg.reply.append(qreply)
+            print "Gathered replies from DB. Sending back to requester. TODO"
+        
         else:
-            print "Wrong or empty message recvd on subscriber port. Expected submission (" + \
-                str(msg_pb2.MessageType.SUBMISSION) + ") got " +                             \
+            print "Wrong or empty message recvd on subscriber port. Expected submission or query (" + \
+                str(msg_pb2.MessageType.SUBMISSION) + " or " +                               \
+                str(msg_pb2.MessageType.QUERY) + ")  got " +                                 \
                 str(msg.type) + " number of parts (should be > 0) " +                        \
-                str(len(msg.submissionRequest)) 
+                str(len(msg.submissionRequest)) + " / " + str(len(msg.queryRequest)) 
                  
-    unregister(req, apikey, cifrouter, myid)
+    cf.unregister()
     
 except KeyboardInterrupt:
-    ctrlc(req, apikey, cifrouter, myid)
+    cif_objs.send() # flush
+    cf.ctrlc()
 except IOError as e:
     print "I/O error({0}): {1}".format(e.errno, e.strerror)
 except KeyError as e:
     print "PB KeyError: ", e
     traceback.print_exc(file=sys.stdout)
 except Exception as inst:
-    print "Unexpected error: ", sys.exc_info()[0]
-     
+    print "Unexpected error: ", sys.exc_info()[0], " ", sys.exc_info()[1], " "
+    traceback.print_tb(sys.exc_info()[2])
+except TTransportException as e:
+    print "Can't connect to HBase"
     
